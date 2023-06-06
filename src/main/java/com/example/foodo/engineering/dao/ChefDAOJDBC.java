b1f18aa86790fa0f4161957989e482dfdfb96047
carlo-fioramanti <carlomariafioramanti@gmail.com>
2023-06-06 15:21:19 +0200

    caso d'uso: persistenza sul file system e sistemazione switch di alcune interfacce

---
+++ src/main/java/com/example/foodo/engineering/dao/ChefDAOJDBC.java
@@ -0,0 +1,86 @@
+package com.example.foodo.engineering.dao;
+
+import com.example.foodo.engineering.dao.queries.BasicQueries;
+import com.example.foodo.engineering.exception.ConnectionDbException;
+import com.example.foodo.engineering.exception.NotFoundException;
+import com.example.foodo.model.ChefModel;
+import com.example.foodo.engineering.connection.ConnectionDB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ChefDAOJDBC  extends ChefDAO{
+
+    private static final String CUISINE="typeOfCuisine";
+    private static final String WORKPLACE="workplace";
+    private static final String EMAIL="email";
+    private static final String NUMBER="number";
+    private static final String LOCATION="location";
+    private static final String PATH="path";
+
+    //private static final Integer CHEFTYPE="";
+
+
+
+    public ChefDAOJDBC(){}
+
+    @Override
+    public ChefModel retrieveChefByUsername(String username) {
+        Statement stmt;
+        ChefModel chefModel=null;
+        try{
+            stmt=ConnectionDB.getConnection();
+
+            ResultSet resultSet= BasicQueries.selectChefByUsername(stmt, username);
+
+            if(!resultSet.first()){
+                throw new NotFoundException("No chef find with the username: " + username);
+            }
+
+            resultSet.first();
+            do {
+
+                chefModel = setChefInfo(username, resultSet);
+            }while(resultSet.next());
+            resultSet.close();
+
+        }
+        catch(SQLException | ConnectionDbException | NotFoundException e){
+            e.printStackTrace();
+        }
+        return chefModel;
+    }
+
+    private static ChefModel setChefInfo(String username, ResultSet resultSet){
+        ChefModel chefModel=null;
+        try{
+            //int chefTypeOfUser=resultSet.getInt(CHEFTYPE);
+            String typeOfCuisine=resultSet.getString(CUISINE);
+            String workplace=resultSet.getString(WORKPLACE);
+            String email = resultSet.getString(EMAIL);
+            String number = resultSet.getString(NUMBER);
+            String location = resultSet.getString(LOCATION);
+            String path= resultSet.getString(PATH);
+            chefModel=new ChefModel(username, typeOfCuisine, workplace, 1, email, number, location, path);
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return chefModel;
+    }
+
+    public static void updateImage(String path, String username) {
+        Statement statement;
+
+        try{
+            statement = ConnectionDB.getConnection();
+            //result set query
+            BasicQueries.updateChefImage(statement, path, username);
+
+        }catch (SQLException | ConnectionDbException e) {
+            e.printStackTrace();
+        }
+    }
+}
